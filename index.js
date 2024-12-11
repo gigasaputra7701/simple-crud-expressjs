78336fb558d8a2e49433b5a8780b7ef877abf3a2
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-12-11 22:09:49 +0700

    simple crud with expressjs

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const rupiah = require("./utils/formatIdr");
+const methodOverride = require("method-override");
+
+const app = express();
+
+const Product = require("./models/product");
+
+// Connect to MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/shop_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Connection error:", err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else{
+    const products = await Product.find({});
+    res.render("products/index", { products, category:'All' });
+  }
+});
+
+app.get("/products/create", (req, res) => {
+  res.render("products/create");
+});
+
+app.post("/products", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product, rupiah });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+  res.redirect(`/products/${id}`);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/products/");
+});
+
+app.listen(3000, () => {
+  console.log(`Shop App listening on http://127.0.0.1:3000`);
+});
