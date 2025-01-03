7ff9f68ca0013b01b21ba7f6a0d7f805fa5ab526
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2025-01-03 21:48:12 +0700

    Custom ErrorHandler

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const rupiah = require("./utils/formatIdr");
+const ErrorHandler = require("./utils/ErrorHandler");
 const methodOverride = require("method-override");
 
 const app = express();
@@ -33,14 +34,15 @@ app.get("/products", async (req, res) => {
   if (category) {
     const products = await Product.find({ category });
     res.render("products/index", { products, category });
-  } else{
+  } else {
     const products = await Product.find({});
-    res.render("products/index", { products, category:'All' });
+    res.render("products/index", { products, category: "All" });
   }
 });
 
 app.get("/products/create", (req, res) => {
-  res.render("products/create");
+  throw new ErrorHandler("This is a custom error", 503);
+  // res.render("products/create");
 });
 
 app.post("/products", async (req, res) => {
@@ -49,10 +51,14 @@ app.post("/products", async (req, res) => {
   res.redirect(`/products/${product._id}`);
 });
 
-app.get("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render("products/show", { product, rupiah });
+app.get("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render("products/show", { product, rupiah });
+  } catch (error) {
+    next(new ErrorHandler("Product Not Found", 404));
+  }
 });
 
 app.get("/products/:id/edit", async (req, res) => {
@@ -61,16 +67,25 @@ app.get("/products/:id/edit", async (req, res) => {
   res.render("products/edit", { product });
 });
 
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
-  res.redirect(`/products/${id}`);
+app.put("/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${id}`);
+  } catch (error) {
+    next(new ErrorHandler("Product failed to update", 403));
+  }
 });
 
 app.delete("/products/:id", async (req, res) => {
   const { id } = req.params;
   await Product.findByIdAndDelete(id);
   res.redirect("/products/");
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
 });
 
 app.listen(3000, () => {
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Product = require("./models/product");
+const Product = require("../models/product");
 
 mongoose
   .connect("mongodb://127.0.0.1/shop_db")
